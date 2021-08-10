af146f45a94aac6351535171c7fdd97ecd047679
Nes09 <nesnes569@gmail.com>
2021-08-09 23:40:57 -0400

    working on project day1

--- src/day1.java
+++ src/day1.java
@@ -2,5 +2,6 @@ public class day1 {
     public static void main(String[] args) {
         System.out.println("Hello Marina");
         System.out.println("Cybertek");
+        System.out.println("Let's work on this project");
     }
 }
