6dbd9ea51fe8f4e3d53d155baaa9f8d8df56b609
Nes09 <nesnes569@gmail.com>
2021-08-09 23:58:04 -0400

    2 commit

--- src/day1.java
+++ src/day1.java
@@ -7,6 +7,7 @@ public class day1 {
         System.out.println("Let's write a good code");
         System.out.println("Writing the code is fun=)");
         // 1 commit
+        //2 commit
 
 
     }
