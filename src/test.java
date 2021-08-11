f0a7156166daa8e8bd52d618dd3a44129e6273c4
Marina Vel <marinavelitskaia@gmail.com>
2021-08-10 23:14:43 -0400

    commit

---
+++ src/test.java
@@ -0,0 +1,23 @@
+public class test {
+
+    public void method1(){
+        System.out.println("a");
+    }
+
+    public test(){
+        System.out.println("b");
+    }
+
+    {
+        System.out.println("c");
+    }
+     }
+
+
+class test2 {
+       public static void main(String[]args){
+test test = new test();
+test.method1();
+
+        }
+        }
