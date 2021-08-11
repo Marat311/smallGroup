2134294b88ab0990285eee4ef97df40e8fc809f8
inesa <inesa.yun@yahoo.com>
2021-08-10 22:56:47 -0400

    created carpet

---
+++ src/Carpet.java
@@ -0,0 +1,12 @@
+public class Carpet {
+
+    public String color;
+    public int size;
+    public double price;
+
+    public static boolean isPersian;
+
+    public Carpet(String color){
+        this.color=color;
+    }
+}
