41a7d853345f9308419c878b0f83ca1978389c68
Marina Vel <marinavelitskaia@gmail.com>
2021-08-10 23:22:58 -0400

    class

--- src/test.java
+++ src/test.java
@@ -2,22 +2,23 @@ public class test {
 
     public void method1(){
         System.out.println("a");
-    }
+    } // method
 
     public test(){
         System.out.println("b");
-    }
+    } //constructor
 
-    {
-        System.out.println("c");
+    static {    System.out.println("c"); //block
     }
      }
 
 
-class test2 {
-       public static void main(String[]args){
-test test = new test();
-test.method1();
+       class test2 {
 
+       public static void main(String[]args){
+
+       test test = new test();
+       test.method1();
+//c b a
         }
         }
